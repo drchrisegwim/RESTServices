8249c08a22906c48a73c52b0bf7d4a1528a2918f
CHRISTIAN EGWIM <christian.egwim@venturegardengroup.com>
2018-03-06 18:08:42 +0100

    INITIAL COMMIT

---
+++ src/SampleEndPoint/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SampleEndPoint.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpPost]
+        public bool AddEmpDetails()
+        {
+            return true;
+        }
+
+        [HttpGet]
+        public string GetEmpDetails()
+        {
+            return "Christian Egwim";
+        }
+
+        [HttpDelete]
+        public string DeleteEmpDetails(string id)
+        {
+            return "Employ detail have been deleted having " + id;
+        }
+
+        [HttpPut]
+
+        public string UpdateEmpDetails(string name, string id)
+        {
+            return "Employee Detail Updated with " + name + "and Id " + id;
+        }
+    }
+}
---
+++ src/SampleEndPoint/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleEndPoint.Global" Language="C#" %>
